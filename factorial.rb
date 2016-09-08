7d9890a81ffc7cfa060d977e13c1fe5c6d6eb616
Koji Hiramatsu <mazz1796@gmail.com>
2016-09-08 10:40:30 +0800

    factorial

--- factorial.rb
+++ factorial.rb
@@ -1,9 +1,24 @@
 # Implement an iterative version of the factorial function
+require "byebug"
 def factorial_iterative(n)
+	if n == 0
+		1
+	else
+		
+		return (1..n).inject {|sum, num| sum*num}
+		
+	end
 end
 
-# Implement a recursive version of the factorial function
+#Implement a recursive version of the factorial function
 def factorial_recursive(n)
+	if n == 0
+		1
+	else
+		return n*factorial_recursive(n-1)
+	end
+	
 end
 
+puts factorial_iterative(9)
 puts factorial_recursive(9) == 362880
